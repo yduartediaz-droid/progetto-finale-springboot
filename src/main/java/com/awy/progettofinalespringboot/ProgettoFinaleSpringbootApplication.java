28eebbb02ecb76c02049146469b5b12e9550a871
yduartediaz-droid <yduartediaz@studenti.laziodigital.it>
2026-07-08 16:10:17 +0200

    init

---
+++ src/main/java/com/awy/progettofinalespringboot/ProgettoFinaleSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.awy.progettofinalespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoFinaleSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProgettoFinaleSpringbootApplication.class, args);
+    }
+
+}
