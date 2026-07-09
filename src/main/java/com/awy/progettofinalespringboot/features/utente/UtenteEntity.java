01fc9e43c558ab578b2562eb7106d9b8ca8d9107
argeldev21 <argeldev21@gmail.com>
2026-07-09 10:54:38 +0200

    feat [edit]: UtenteEntity, pom.xml & application.properties

--- src/main/java/com/awy/progettofinalespringboot/features/utente/UtenteEntity.java
+++ src/main/java/com/awy/progettofinalespringboot/features/utente/UtenteEntity.java
@@ -1,4 +1,23 @@
 package com.awy.progettofinalespringboot.features.utente;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "tb_utenti")
 public class UtenteEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String username;
+
+    @Column(nullable = false)
+    private int puntiTotali;
 }
