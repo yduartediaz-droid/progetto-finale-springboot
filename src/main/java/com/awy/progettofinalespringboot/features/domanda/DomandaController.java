4f4e250698b20d570c3cdee9585924496c5ade02
yduartediaz-droid <yduartediaz@studenti.laziodigital.it>
2026-07-09 13:53:28 +0200

    feat [add] : DomandaController, DomandaService.java

--- src/main/java/com/awy/progettofinalespringboot/features/domanda/DomandaController.java
+++ src/main/java/com/awy/progettofinalespringboot/features/domanda/DomandaController.java
@@ -1,4 +1,27 @@
 package com.awy.progettofinalespringboot.features.domanda;
 
+import com.awy.progettofinalespringboot.features.domanda.service.DomandaService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/domande")
 public class DomandaController {
+
+    private final DomandaService service;
+
+    public DomandaController(DomandaService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<DomandaEntity> getAll() {
+        return service.getAll();
+    }
+
+    @PostMapping
+    public DomandaEntity create(@RequestBody DomandaEntity domanda) {
+        return service.create(domanda);
+    }
 }
--- src/main/java/com/awy/progettofinalespringboot/features/domanda/DomandaService.java
+++ src/main/java/com/awy/progettofinalespringboot/features/domanda/DomandaService.java
@@ -1,4 +1,27 @@
 package com.awy.progettofinalespringboot.features.domanda;
 
-public class DomandaService {
+import com.awy.progettofinalespringboot.features.domanda.DomandaEntity;
+import com.awy.progettofinalespringboot.features.domanda.repository.DomandaRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DomandaServiceImpl implements DomandaService {
+
+    private final DomandaRepository repository;
+
+    public DomandaServiceImpl(DomandaRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public List<DomandaEntity> getAll() {
+        return repository.findAll();
+    }
+
+    @Override
+    public DomandaEntity create(DomandaEntity domanda) {
+        return repository.save(domanda);
+    }
 }
