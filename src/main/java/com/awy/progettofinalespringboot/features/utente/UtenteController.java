b2ca7d493900c66ac8ec01a6a5ac9d8e8a112cdf
argeldev21 <argeldev21@gmail.com>
2026-07-13 22:40:29 +0200

    feat [add]: WebConfig

--- src/main/java/com/awy/progettofinalespringboot/features/utente/UtenteController.java
+++ src/main/java/com/awy/progettofinalespringboot/features/utente/UtenteController.java
@@ -7,7 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin(origins = "*")
+
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
 public class UtenteController {
